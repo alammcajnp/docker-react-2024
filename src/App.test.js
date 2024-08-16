c9088e5b596bf27e58dd43d9a3d437832f08b4ec
alammcajnp <alamjnp12@gmail.com>
2024-08-16 14:40:33 +0530

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/How/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders again learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/How/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("ALam", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/How/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test("Ok it again Alam", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/How/i);
   expect(linkElement).toBeInTheDocument();
 });
